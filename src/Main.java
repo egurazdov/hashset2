65be5259638a194d2647e7dc50eb6b1cd16b3cd3
Egurazdov <egurazdov@yandex.ru>
2024-10-13 13:53:39 +0300

    Second

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.util.HashSet;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        HashSet<Integer> set = new HashSet<>();
+        set.add(1);
+        set.add(10);
+        set.add(100);
+        set.add(15);
+        set.add(16);
+        set.add(17);
+        set.add(18);
+
+            for (Integer integer : set) {
+                if ((integer%2 == 0) & (integer > 15)) {
+                    System.out.println(integer);
+                } else {
+                    System.out.println(integer/2);
+                }
+            }
+        }
+
+
     }
-}
